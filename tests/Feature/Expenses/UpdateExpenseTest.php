d8f6d662c10394449774dd21c238922efe4d6242
Álvaro Navarro <alvaro@thesnakebite.es>
2026-08-17 17:04:38 +0200

    test: add guest and unverified user tests for expense update

--- tests/Feature/Expenses/UpdateExpenseTest.php
+++ tests/Feature/Expenses/UpdateExpenseTest.php
@@ -40,3 +40,55 @@ it('allows the expense owner to update an expense', function () {
         'id' => $expense->id,
     ]);
 });
+
+it('does not allow guest to update expense', function () {
+    $user = User::factory()->create([
+        'email_verified_at' => now(),
+    ]);
+
+    $budget = Budget::factory()->for($user)->create([
+        'type' => 'general',
+    ]);
+
+    $expense = Expense::factory()->for($budget)->create();
+
+    $response = $this->put(route('budgets.expenses.update', [$budget, $expense]), [
+        'name' => 'Gasto actualizado',
+        'amount' => 200.05,
+        'category' => 'food',
+    ]);
+
+    $response->assertRedirect(route('login'));
+    $this->assertDatabaseMissing('expenses', [
+        'name' => 'Gasto actualizado',
+        'amount' => 200.05,
+        'category' => 'food',
+        'id' => $expense->id,
+    ]);
+});
+
+it('does not allow unverified users to update expenses', function () {
+    $user = User::factory()->create([
+        'email_verified_at' => null,
+    ]);
+
+    $budget = Budget::factory()->for($user)->create([
+        'type' => 'general',
+    ]);
+
+    $expense = Expense::factory()->for($budget)->create();
+
+    $response = $this->actingAs($user)->put(route('budgets.expenses.update', [$budget, $expense]), [
+        'name' => 'Gasto actualizado',
+        'amount' => 200.05,
+        'category' => 'food',
+    ]);
+
+    $response->assertRedirect(route('verification.notice'));
+    $this->assertDatabaseMissing('expenses', [
+        'name' => 'Gasto actualizado',
+        'amount' => 200.05,
+        'category' => 'food',
+        'id' => $expense->id,
+    ]);
+});
